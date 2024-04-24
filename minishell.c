a29131d4950b72ffa44be6e7a812d012abb65edc
saad <shamdoun1337@gmail.com>
2024-04-24 15:58:00 +0100

    handled redirections. < >> << are all interpreted according to the standard behaviour on bash

--- minishell.c
+++ minishell.c
@@ -10,11 +10,19 @@ int main(int argc, char **argv, char **env)
     // minishell->all_status = NULL;
     minishell->env = env;
     t_input *new_input = ft_lstnew_input();
-    new_input->command_name = "cat";
-    new_input->args = NULL;
+    new_input->command_name = "echo";
+    new_input->args = "fda fdsamds";
     ft_lst_add_input_back(&minishell->all_input, new_input);
-    t_file *file1 = ft_lstnew_file(NULL, 4, "da");
+    t_file *file1 = ft_lstnew_file("f1", 1, "da");
+    t_file *file2 = ft_lstnew_file("f2", 2, "da");
+    t_file *file3 = ft_lstnew_file("Makefile", 3, "da");
+    t_file *file4 = ft_lstnew_file(NULL, 4, "da");
+    t_file *file5 = ft_lstnew_file("f1", 1, "da");
     ft_lst_add_file_back(&minishell->all_input->all_files, file1);
+    ft_lst_add_file_back(&minishell->all_input->all_files, file2);
+    ft_lst_add_file_back(&minishell->all_input->all_files, file3);
+    ft_lst_add_file_back(&minishell->all_input->all_files, file4);
+    ft_lst_add_file_back(&minishell->all_input->all_files, file5);
     execute_input(minishell);
     free(minishell);
 }
